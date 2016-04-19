3e0b87e358e2c6e4ddbd5f553a2003a2bc31106a
Hanna <Hanna@dhcp-gs-685.redrover.cornell.edu>
2016-04-19 14:32:40 -0400

    includes aggregate amounts for eac field as json

---
+++ aggregate.js
@@ -0,0 +1,12 @@
+var data = 
+[
+  {
+    "AvgTuition": 39254.02,
+    "AvgCostLiving": 13389.76,
+    "AvgStudentBody": 12993.92,
+    "AvgOnFinAid": "47.15%",
+    "AvgGrantAmount": 29810.06,
+    "AvgStartSalary": 55620,
+    "AvgMidSalary": 102382
+  }
+];
